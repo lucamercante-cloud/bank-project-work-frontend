b654b19df0b1f4fbe12956768feb79fbd1c93881
thefgoat <giovanni.lari@allievi.itsdigitalacademy.com>
2026-09-21 14:48:55 +0200

    creation of service api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
 
