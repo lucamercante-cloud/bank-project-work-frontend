561e55b45a2ddcac64185367ea1d73c6af662306
thefgoat <giovanni.lari@allievi.itsdigitalacademy.com>
2026-09-23 09:35:27 +0200

    navbartestoteronica

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes/AppRoutes";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <main className="min-h-screen bg-slate-950 flex flex-col justify-start">
-                <AppRoutes />
-            </main>
-        </BrowserRouter>
+import { AppRoutes } from "./routes/AppRoutes";
+import { useLocation } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+ 
+function App(){
+const location = useLocation();
+   const isloginoregister = location.pathname === "/login" || location.pathname === "/register";
+   return (
+        <main className="min-h-screen bg-slate-950 flex flex-col justify-start">
+            {!isloginoregister && <Navbar />}
+
+            <AppRoutes />
+        </main>
     );
 }
+
+
 
 export default App;
---
+++ src/components/layout/RechargeLayout.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+interface RechargeLayoutProps {
+    children: ReactNode;
+}
+
+export const RechargeLayout = ({ children }: RechargeLayoutProps) => {
+    return(
+       return (
+        <div className="min-h-screen bg-[#060a12] text-slate-100 flex flex-col justify-between selection:bg-[#59DE00] selection:text-black">
+    )
