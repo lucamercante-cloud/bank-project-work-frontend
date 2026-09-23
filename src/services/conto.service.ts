8493af1126af8c5a2a62a28aa3700a0f8938dd91
lucamercante-cloud <luca.mercante@allievi.itsdigitalacademy.com>
2026-09-23 17:22:48 +0200

    api password and file

--- src/services/conto.service.ts
+++ src/services/conto.service.ts
@@ -5,3 +5,8 @@ export async function me(): Promise<ContoCorrente> {
     const response = await api.get<ContoCorrente>("/conto-corrente/me");
     return response.data;
 }
+
+export async function cambiaPassword(): Promise<undefined> {
+    const response = await api.patch<undefined>("/conto-corrente/password");
+    return response.data;
+}
